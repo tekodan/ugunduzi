128f91edc6b2aea7cff81d3dc62339ffb91f51b4
ojovoz <cubo23@yahoo.com>
2018-03-08 13:09:44 +0100

    gitignore
    gitignore

--- includes/init_database.php
+++ includes/init_database.php
@@ -2,9 +2,9 @@
 //initialize dbConnection
 function initDB() {
 	$host="localhost";
-	$db="your_database";
-	$db_user="your_user";
-	$db_pass="your_password";
+	$db="ugunduzi";
+	$db_user="mysautiyaw";
+	$db_pass="j8bA0y11";
 	$dbh=mysqli_connect($host, $db_user, $db_pass, $db);
 	return $dbh;
 }
