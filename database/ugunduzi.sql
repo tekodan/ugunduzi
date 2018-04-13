8267a3b645feae4532d39756f7bafa1dc6aa710a
ojovoz <cubo23@yahoo.com>
2018-04-13 12:41:58 +0200

    App data
    App data, changes to database

--- database/ugunduzi.sql
+++ database/ugunduzi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net
 --
 -- Servidor: 192.168.86.55
--- Tiempo de generación: 26-03-2018 a las 18:37:10
+-- Tiempo de generación: 13-04-2018 a las 12:39:56
 -- Versión del servidor: 5.5.57-0+deb7u1-log
 -- Versión de PHP: 5.3.29-1~dotdeb.0
 
@@ -51,8 +51,24 @@ INSERT INTO `crop` (`crop_id`, `crop_name`, `crop_variety`) VALUES
 CREATE TABLE IF NOT EXISTS `data_item` (
   `data_item_id` int(10) unsigned NOT NULL,
   `data_item_name` varchar(100) NOT NULL,
-  `data_item_default_units_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `data_item_default_units_id` int(10) unsigned NOT NULL,
+  `data_item_type` int(10) unsigned NOT NULL COMMENT '0=number, 1=date, 2=cost',
+  `is_crop_specific` tinyint(1) NOT NULL,
+  `is_treatment_specific` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `data_item`
+--
+
+INSERT INTO `data_item` (`data_item_id`, `data_item_name`, `data_item_default_units_id`, `data_item_type`, `is_crop_specific`, `is_treatment_specific`) VALUES
+(1, 'Land preparation', 0, 1, 0, 0),
+(2, 'Planting', 0, 1, 1, 0),
+(3, 'Harvesting', 0, 1, 1, 0),
+(4, 'Yield', 1, 0, 1, 0),
+(5, 'Application', 0, 1, 0, 1),
+(6, 'Costs', 0, 2, 1, 1),
+(7, 'Sales', 0, 2, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -67,7 +83,20 @@ CREATE TABLE IF NOT EXISTS `farm` (
   `farm_size_acres` int(10) unsigned NOT NULL,
   `farm_date_created` date NOT NULL,
   `parent_farm_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `farm`
+--
+
+INSERT INTO `farm` (`farm_id`, `user_id`, `farm_name`, `farm_size_acres`, `farm_date_created`, `parent_farm_id`) VALUES
+(12, 2, 'Shamba 2', 1, '2018-03-29', 0),
+(13, 2, 'Shamba 3', 1, '2018-03-29', 0),
+(14, 2, 'Shamba 4', 1, '2018-04-02', 0),
+(15, 2, 'Shamba 5', 1, '2018-04-04', 0),
+(16, 4, 'Shamba 1', 1, '2018-04-09', 0),
+(17, 2, 'Shamba 6', 1, '2018-04-09', 0),
+(18, 2, 'Shamba 7', 1, '2018-04-09', 0);
 
 -- --------------------------------------------------------
 
@@ -104,7 +133,36 @@ CREATE TABLE IF NOT EXISTS `plot` (
   `plot_crop2` int(10) unsigned NOT NULL,
   `plot_treatment1` int(10) unsigned NOT NULL,
   `plot_treatment2` int(10) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `plot`
+--
+
+INSERT INTO `plot` (`plot_id`, `internal_plot_id`, `farm_id`, `plot_x`, `plot_y`, `plot_w`, `plot_h`, `plot_crop1`, `plot_crop2`, `plot_treatment1`, `plot_treatment2`) VALUES
+(55, 0, 12, 0, 0, 2, 4, 1, 0, 0, 0),
+(56, 1, 12, 2, 0, 2, 4, 1, 3, 0, 0),
+(57, 0, 13, 0, 0, 1, 1, 1, 3, 2, 0),
+(58, 0, 14, 0, 0, 1, 3, 1, 0, 1, 0),
+(59, 1, 14, 1, 0, 1, 4, 2, 0, 2, 0),
+(60, 2, 14, 2, 0, 2, 4, 3, 0, 0, 0),
+(61, 3, 14, 0, 3, 1, 1, 0, 0, 0, 0),
+(62, 0, 15, 1, 1, 2, 2, 1, 0, 0, 0),
+(63, 1, 15, 0, 0, 1, 1, 1, 0, 0, 0),
+(64, 2, 15, 1, 0, 1, 1, 1, 0, 2, 0),
+(65, 3, 15, 2, 0, 1, 1, 1, 0, 1, 0),
+(66, 4, 15, 3, 0, 1, 1, 1, 0, 2, 1),
+(67, 5, 15, 0, 1, 1, 1, 1, 0, 2, 0),
+(68, 6, 15, 3, 1, 1, 1, 1, 0, 1, 0),
+(69, 7, 15, 0, 2, 1, 1, 1, 0, 2, 1),
+(70, 8, 15, 3, 2, 1, 1, 1, 0, 0, 0),
+(71, 9, 15, 0, 3, 1, 1, 1, 0, 1, 0),
+(72, 10, 15, 1, 3, 1, 1, 1, 0, 2, 0),
+(73, 11, 15, 2, 3, 1, 1, 1, 0, 2, 1),
+(74, 12, 15, 3, 3, 1, 1, 1, 0, 1, 0),
+(75, 0, 16, 0, 0, 2, 2, 3, 0, 2, 0),
+(78, 0, 17, 1, 1, 3, 3, 3, 0, 1, 2),
+(79, 0, 18, 0, 0, 3, 2, 1, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -135,7 +193,15 @@ INSERT INTO `treatment` (`treatment_id`, `treatment_name`, `treatment_category`)
 CREATE TABLE IF NOT EXISTS `units` (
   `units_id` int(10) unsigned NOT NULL,
   `units_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `units`
+--
+
+INSERT INTO `units` (`units_id`, `units_name`) VALUES
+(1, 'Kg'),
+(2, 'Baskets');
 
 -- --------------------------------------------------------
 
@@ -160,7 +226,8 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`user_id`, `user_name`, `user_alias`, `user_password`, `user_mobile`, `user_group`, `user_association`, `user_location`) VALUES
 (1, 'Test user', 'test', 'test', '', '', '', ''),
-(2, '', 'eugenio', 'cubo23', '', '', '', '');
+(2, 'Eugenio Tisselli', 'eugenio', 'cubo23', '', '', '', ''),
+(4, '', 'eg', 'gh', '', '', '', '');
 
 --
 -- Índices para tablas volcadas
@@ -227,12 +294,12 @@ ALTER TABLE `crop`
 -- AUTO_INCREMENT de la tabla `data_item`
 --
 ALTER TABLE `data_item`
-  MODIFY `data_item_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `data_item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT de la tabla `farm`
 --
 ALTER TABLE `farm`
-  MODIFY `farm_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `farm_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT de la tabla `log`
 --
@@ -242,7 +309,7 @@ ALTER TABLE `log`
 -- AUTO_INCREMENT de la tabla `plot`
 --
 ALTER TABLE `plot`
-  MODIFY `plot_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+  MODIFY `plot_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=80;
 --
 -- AUTO_INCREMENT de la tabla `treatment`
 --
@@ -252,7 +319,7 @@ ALTER TABLE `treatment`
 -- AUTO_INCREMENT de la tabla `units`
 --
 ALTER TABLE `units`
-  MODIFY `units_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `units_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
